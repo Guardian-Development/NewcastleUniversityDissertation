f5c5c5db6dd11b7d06ea1e0fec53c19373c69e25
Joe Honour (UG) <b3029153@newcastle.ac.uk>
2017-12-03 10:49:54 +0000

    * adding opencv modules

--- VideoProcessingService/setup_env.sh
+++ VideoProcessingService/setup_env.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Script to setup a ubuntu 16.04 dev environment for the VideoProcessingService
 
-# update system
+# Update system
+sudo apt-get -y update
 sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
 
-# install pip
+# Install pip
 sudo apt-get install -y python3-pip
 
 # Dependencies for face_recognition package
@@ -39,43 +42,76 @@ cd ~ && \
     python3 setup.py install --yes USE_AVX_INSTRUCTIONS
 
 # Dependencies for OpenCV
-sudo apt-get autoremove libopencv-dev python-opencv
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-sudo apt-get -y autoremove
-
-# 1. KEEP UBUNTU OR DEBIAN UP TO DATE
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-sudo apt-get -y autoremove
-
-# 2. INSTALL THE DEPENDENCIES
+sudo apt-get autoremove \ 
+    libopencv-dev \ 
+    python-opencv
 
 # Build tools:
-sudo apt-get install -y build-essential cmake
+sudo apt-get install -y \ 
+    build-essential \
+    cmake \ 
+    git \ 
+    pkg-config \ 
+    unzip \ 
+    wget
 
-# GUI (if you want to use GTK instead of Qt, replace 'qt5-default' with 'libgtkglext1-dev' and remove '-DWITH_QT=ON' option in CMake):
-sudo apt-get install -y qt5-default libvtk6-dev
+# GUI
+sudo apt-get install -y \ 
+    qt5-default \
+    libvtk6-dev
 
 # Media I/O:
-sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+sudo apt-get install -y \
+    zlib1g-dev \ 
+    libjpeg-dev \ 
+    libwebp-dev \ 
+    libpng-dev \ 
+    libtiff5-dev \ 
+    libjasper-dev \ 
+    libopenexr-dev \ 
+    libgdal-dev \ 
+    libpng12-dev
 
 # Video I/O:
-sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+sudo apt-get install -y \ 
+    libdc1394-22-dev \ 
+    libavcodec-dev \ 
+    libavformat-dev \ 
+    libswscale-dev \ 
+    libtheora-dev \ 
+    libvorbis-dev \ 
+    libxvidcore-dev \ 
+    libx264-dev \
+    yasm \
+    libopencore-amrnb-dev \
+    libopencore-amrwb-dev \ 
+    libv4l-dev \ 
+    libxine2-dev
 
 # Parallelism and linear algebra libraries:
 sudo apt-get install -y libtbb-dev libeigen3-dev
 
+# Optimisation libraries
+sudo apt-get install -y \ 
+    libatlas-base-dev \ 
+    gfortran
+
 # Python:
-sudo apt-get install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
+sudo apt-get install -y \ 
+    python-dev \
+    python-tk \ 
+    python-numpy \
+    python3-dev \ 
+    python3-tk \ 
+    python3-numpy
 
-# 3. INSTALL THE LIBRARY (YOU CAN CHANGE '3.2.0' FOR THE LAST STABLE VERSION)
+# Install OpenCV contrib modules
+wget https://github.com/opencv/opencv_contrib/archive/3.3.0.zip
+unzip 3.3.0.zip
+rm 3.3.0.zip
+mv opencv_contrib-3.3.0 OpenCVModules
 
-sudo apt-get install -y unzip wget
+# Install OpenCV
 wget https://github.com/opencv/opencv/archive/3.3.0.zip
 unzip 3.3.0.zip
 rm 3.3.0.zip
@@ -83,7 +119,16 @@ mv opencv-3.3.0 OpenCV
 cd OpenCV
 mkdir build
 cd build
-cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=OFF -DENABLE_PRECOMPILED_HEADERS=OFF ..
+cmake CMAKE_BUILD_TYPE=RELEASE \ 
+    -D OPENCV_EXTRA_MODULES_PATH=~/OpenCVModules/modules \
+    -D WITH_QT=ON \ 
+    -D WITH_OPENGL=ON \ 
+    -D FORCE_VTK=ON \ 
+    -D WITH_TBB=ON \
+    -D WITH_GDAL=ON \ 
+    -D WITH_XINE=ON \ 
+    -D BUILD_EXAMPLES=OFF \ 
+    -D ENABLE_PRECOMPILED_HEADERS=OFF ..
 make -j4
 sudo make install
 sudo ldconfig
