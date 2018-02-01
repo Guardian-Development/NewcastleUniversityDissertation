cdc1f782b25f2429af2d45a78e74616d01fb7b0f
Joe <joehonour@hotmail.co.uk>
2018-02-01 16:38:58 +0000

    * first attempt at encoding with json, not ran it.

--- VideoProcessingService/message_sending/encoder.py
+++
@@ -1,6 +0,0 @@
-from typing import Callable, List, Tuple
-
-Encoder = Callable[[List[Tuple[float, float, float, float,str]]], str]
-
-def encode_detected_objects(detected_objects: List[Tuple[float, float, float, float, str]]) -> str:
-    pass
