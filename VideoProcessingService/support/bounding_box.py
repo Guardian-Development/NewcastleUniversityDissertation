32629897581a0428a942217af7626f844d1fe1a8
Joe <joehonour@hotmail.co.uk>
2018-02-05 17:04:02 +0000

    object tracking initial code cleanup

---
+++ VideoProcessingService/support/bounding_box.py
@@ -0,0 +1,33 @@
+from typing import Tuple
+
+
+def bounding_boxes_collide(box1: Tuple[float, float, float, float],
+                           box2: Tuple[float, float, float, float]) -> bool:
+    box1_left_x, box1_bottom_y, box1_right_x, box1_top_y = box1
+    box2_left_x, box2_bottom_y, box2_right_x, box2_top_y = box2
+
+    return (box1_left_x <= box2_right_x and
+            box1_right_x >= box2_left_x and
+            box1_top_y >= box2_bottom_y and
+            box1_bottom_y <= box2_top_y)
+
+
+def intersection_over_union(box1: Tuple[float, float, float, float],
+                            box2: Tuple[float, float, float, float]) -> float:
+    box1_left_x, box1_bottom_y, box1_right_x, box1_top_y = box1
+    box2_left_x, box2_bottom_y, box2_right_x, box2_top_y = box2
+
+    intersection_left_x = max(box1_left_x, box2_left_x)
+    intersection_bottom_y = max(box1_bottom_y, box2_bottom_y)
+    intersection_right_x = min(box1_right_x, box2_right_x)
+    intersection_top_y = min(box1_top_y, box2_top_y)
+
+    intersection_area = (intersection_right_x - intersection_left_x + 1) * \
+                        (intersection_top_y - intersection_bottom_y + 1)
+
+    box1_area = (box1_right_x - box1_left_x + 1) * \
+                (box1_top_y - box1_bottom_y + 1)
+    box2_area = (box2_right_x - box2_left_x + 1) * \
+                (box2_top_y - box2_bottom_y + 1)
+
+    return intersection_area / float(box1_area + box2_area - intersection_area)
--- VideoProcessingService/video_processing/tracker.py
+++ VideoProcessingService/video_processing/tracker.py
@@ -5,6 +5,7 @@ from typing import List, Tuple
 import cv2
 from numpy import ndarray
 from video_processing.detector import Detector
+from support import bounding_box as bb
 
 
 class Tracker(Detector):
@@ -24,50 +25,6 @@ class Tracker(Detector):
         self.object_detectors = object_detectors
         self.object_trackers = []
 
-    def bounding_boxes_collide(self,
-                               box1: Tuple[float, float, float, float],
-                               box2: Tuple[float, float, float, float]) -> bool:
-
-        box1_left_x, box1_bottom_y, box1_right_x, box1_top_y = box1
-        box2_left_x, box2_bottom_y, box2_right_x, box2_top_y = box2
-
-        return (box1_left_x <= box2_right_x and
-                box1_right_x >= box2_left_x and
-                box1_top_y >= box2_bottom_y and
-                box1_bottom_y <= box2_top_y)
-
-    def bounding_box_inclusive(self,
-                               box1: Tuple[float, float, float, float],
-                               box2: Tuple[float, float, float, float]) -> bool:
-        box1_left_x, box1_bottom_y, box1_right_x, box1_top_y = box1
-        box2_left_x, box2_bottom_y, box2_right_x, box2_top_y = box2
-
-        return (box1_left_x <= box2_left_x and
-                box1_right_x <= box2_right_x and
-                box1_top_y <= box2_top_y and
-                box1_bottom_y <= box2_bottom_y)
-
-    def intersection_over_union(self,
-                                box1: Tuple[float, float, float, float],
-                                box2: Tuple[float, float, float, float]) -> float:
-        box1_left_x, box1_bottom_y, box1_right_x, box1_top_y = box1
-        box2_left_x, box2_bottom_y, box2_right_x, box2_top_y = box2
-
-        intersection_left_x = max(box1_left_x, box2_left_x)
-        intersection_bottom_y = max(box1_bottom_y, box2_bottom_y)
-        intersection_right_x = min(box1_right_x, box2_right_x)
-        intersection_top_y = min(box1_top_y, box2_top_y)
-
-        intersection_area = (intersection_right_x - intersection_left_x + 1) * \
-                            (intersection_top_y - intersection_bottom_y + 1)
-
-        box1_area = (box1_right_x - box1_left_x + 1) * \
-                    (box1_top_y - box1_bottom_y + 1)
-        box2_area = (box2_right_x - box2_left_x + 1) * \
-                    (box2_top_y - box2_bottom_y + 1)
-
-        return intersection_area / float(box1_area + box2_area - intersection_area)
-
     def detect(self, frame: ndarray) -> List[Tuple[float, float, float, float, str]]:
         """Detects objects within a frame
 
@@ -80,43 +37,54 @@ class Tracker(Detector):
             [List[Tuple[float, float, float, float]]] -- [a list of coordinates that people are found at in the image]
         """
 
-        # get the detected objects in the frame
         detected_objects = []
         for detector in self.object_detectors:
             detected_objects.extend(detector.detect(frame))
 
-        # update trackers to get latest locations of objects we know about
         tracked_objects = self.get_tracked_object_locations(frame)
+        new_objects = self.detect_new_objects_and_track(detected_objects, frame, tracked_objects)
 
-        # calculate which objects are new from the detected objects
+        tracked_objects.extend(new_objects)
+        return tracked_objects
+
+    def detect_new_objects_and_track(self,
+                                     detected_objects,
+                                     frame,
+                                     tracked_objects) -> List[Tuple[float, float, float, float, str]]:
         new_objects = []
+
         for detected_object in detected_objects:
             is_new_object = True
 
             # can we find something we already track that shows this is not new
-            for tracked_object, tracker in zip(tracked_objects, self.object_trackers):
-
-                if self.bounding_boxes_collide(detected_object[:4], tracked_object[:4]):
-                    collision_amount = self.intersection_over_union(detected_object[:4], tracked_object[:4])
-                    print("{0}, {1}, collision={2}".format(detected_object, tracked_object, collision_amount))
+            for tracked_object in tracked_objects:
+                if bb.bounding_boxes_collide(detected_object[:4], tracked_object[:4]):
+                    collision_amount = bb.intersection_over_union(detected_object[:4], tracked_object[:4])
                     if collision_amount > 0.2:
                         is_new_object = False
                         break
 
             if is_new_object:
-                new_tracker = cv2.TrackerMIL_create()
-                ok = new_tracker.init(frame, (
-                    detected_object[0],
-                    detected_object[1],
-                    detected_object[2] - detected_object[0],
-                    detected_object[3] - detected_object[1]))
-                if not ok:
-                    continue
-                self.object_trackers.append(new_tracker)
-                new_objects.append(detected_object)
+                could_track = self.initialise_tracker_for_object(frame, detected_object)
+                if could_track:
+                    new_objects.append(detected_object)
 
-        tracked_objects.extend(new_objects)
-        return tracked_objects
+        return new_objects
+
+    def initialise_tracker_for_object(self,
+                                      frame: ndarray,
+                                      detected_object: Tuple[float, float, float, float]) -> bool:
+        new_tracker = cv2.TrackerMIL_create()
+        ok = new_tracker.init(frame, (
+            detected_object[0],
+            detected_object[1],
+            detected_object[2] - detected_object[0],
+            detected_object[3] - detected_object[1]))
+        if not ok:
+            return False
+
+        self.object_trackers.append(new_tracker)
+        return True
 
     def get_tracked_object_locations(self, frame) -> List[Tuple[float, float, float, float, str]]:
         tracked_objects = []
