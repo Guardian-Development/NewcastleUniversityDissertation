9e2ff0074165550d7caaebf9b79da7c9f5dec718
Joe Honour (UG) <b3029153@newcastle.ac.uk>
2018-02-12 13:31:33 +0000

    * this starts kafka successfully yay!!

--- KafkaInfrastructure/main.tf
+++ KafkaInfrastructure/main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 }
 
 # Security group to allow SSH onto a machine 
-resource "aws_security_group" "kafka-security-group" {
-    name = "Kafka Security Group"
+resource "aws_security_group" "kafka-ssh" {
+    name = "Kafka SSH Group"
 
     # Allow port 22 (ssh) connections from any address
     ingress {
@@ -17,7 +17,15 @@ resource "aws_security_group" "kafka-security-group" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    # Allow outgoing network traffic
+    tags {
+        Name = "Kafka-SSH-Group"
+    }
+}
+
+# Security group to allow all outgoing network traffic
+resource "aws_security_group" "kafka-outgoing-traffic" {
+    name = "Kafka All Outgoing Group"
+
     egress {
         from_port = 0
         to_port = 0
@@ -26,15 +34,36 @@ resource "aws_security_group" "kafka-security-group" {
     }
 
     tags {
-        Name = "Kafka-Security-Group"
+        Name = "Kafka-All-Outgoing-Group"
     }
 }
 
+# Security group to allow all incoming network traffic
+resource "aws_security_group" "kafka-incoming-traffic" {
+    name = "Kafka All Incoming Group"
+
+    ingress {
+        from_port = 0 
+        to_port = 0 
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "Kafka-All-Incoming-Group"
+    }
+}
+
+# Kafka machine
 resource "aws_instance" "kafka" {
     # 16.04 LTS, amd64, hvm:ebs-ssd 
     ami = "ami-c1167eb8"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["${aws_security_group.kafka-security-group.id}"]
+    vpc_security_group_ids = [
+        "${aws_security_group.kafka-ssh.id}",
+        "${aws_security_group.kafka-incoming-traffic.id}", 
+        "${aws_security_group.kafka-outgoing-traffic.id}"]
+
     key_name = "${var.kafka_key_pair_name}"
 
     tags {
@@ -47,11 +76,14 @@ resource "aws_instance" "kafka" {
             "sudo apt-get -q -y install default-jre",
             "wget http://mirrors.ukfast.co.uk/sites/ftp.apache.org/kafka/1.0.0/kafka_2.11-1.0.0.tgz",
             "tar -xzf kafka_2.11-1.0.0.tgz",
-            "cd kafka_2.11-1.0.0/",
-            # "export KAFKA_HEAP_OPTS=\"-Xmx500M -Xmx500M\"",
-            # "source ~\\.bashrc",
-            "bin/zookeeper-server-start.sh config/zookeeper.properties &",
-            "bin/kafka-server-start.sh config/server.properties &"
+            "export KAFKA_HEAP_OPTS=\"-Xmx200M -Xmx200M\"",
+            "echo 'Starting Zookeeper'",
+            "nohup kafka_2.11-1.0.0/bin/zookeeper-server-start.sh kafka_2.11-1.0.0/config/zookeeper.properties > ~/zookeeper-logs &",
+            "sleep 5s",
+            "echo 'Starting Kafka'",
+            "nohup kafka_2.11-1.0.0/bin/kafka-server-start.sh kafka_2.11-1.0.0/config/server.properties > ~/kafka-logs &",
+            "sleep 5s",
+            "echo 'Complete Setup'"
         ]
 
         connection {
@@ -67,7 +99,6 @@ resource "aws_instance" "kafka" {
 resource "aws_eip" "kafka-ip" {
   instance = "${aws_instance.kafka.id}"
 }
-
 output "ip" {
   value  = "${aws_eip.kafka-ip.public_ip}"
 }
