fa8ef59cda5dc7f70675b3461effb613e6aa8843
Joe <joehonour@hotmail.co.uk>
2018-03-02 12:54:41 +0000

    * adding initial support package

---
+++ Support/KafkaLocalStorageTool/run.py
@@ -0,0 +1,7 @@
+from command_line_procesor import process_command_line_arguments
+
+if __name__ == '__main__':
+    """
+    Parses command line input, and start support tool
+    """
+    ARGUMENTS = process_command_line_arguments()
