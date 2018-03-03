a1c9841461f5483f0f68a4df61090f010f2d671d
joe <joehonour@hotmail.co.uk>
2018-03-03 14:47:00 +0000

    success read from kafka

--- ActivityAnalysisService/activityanalysisservice/src/main/scala/newcastleuniversity/joehonour/Main.scala
+++ ActivityAnalysisService/activityanalysisservice/src/main/scala/newcastleuniversity/joehonour/Main.scala
@@ -2,10 +2,12 @@ package newcastleuniversity.joehonour
 
 import java.util.Properties
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer010, FlinkKafkaConsumer011}
 
 import scala.io.Source
 import org.json4s._
@@ -28,38 +30,31 @@ object Main {
   def main(args: Array[String]) {
 
     val configuration = CommandLineParser.parseCommandLineArguments(args)
+    val kafkaProperties = new Properties()
+    kafkaProperties.setProperty("bootstrap.servers", configuration.kafkaBootStrapServers)
+    kafkaProperties.setProperty("group.id", "testGroup")
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val raw_file_source = Source
-      .fromFile("../test_json_files/2018-03-02__test_video_one.json")
-      .mkString
-    val json_result = parse(raw_file_source)
-    val parsedResult = json_result.extract[Messages]
-    val source = env.fromCollection(parsedResult.ordered_messages)
+    val kafkaSource = new FlinkKafkaConsumer010[String](configuration.kafkaTopic, new SimpleStringSchema(), kafkaProperties)
 
-    val count = source
-      .flatMap { _.detected_objects }
-        .map { obj => (obj.uuid, 1)}
-        .keyBy { _._1 }
-        .sum(1)
-
-    count.print()
-
+    kafkaSource.setStartFromEarliest()
+    env.addSource(kafkaSource).print()
     env.execute("Test flink job")
 
-    //env.addSource(new FlinkKafkaConsumer081(kafkaTopic, new SimpleStringSchema(), kafkaProperties))
-
-    // run count on how many frames each uuid appeared
-    // read and parse file
-
+//    val env = StreamExecutionEnvironment.getExecutionEnvironment
+//    val raw_file_source = Source
+//      .fromFile("../test_json_files/2018-03-02__test_video_one.json")
+//      .mkString
+//    val json_result = parse(raw_file_source)
+//    val parsedResult = json_result.extract[Messages]
+//    val source = env.fromCollection(parsedResult.ordered_messages)
 //
-//    val flinkSource = env.fromCollection(parsedResult.ordered_messages)
-//    val counts = flinkSource
+//    val count = source
 //      .flatMap { _.detected_objects }
-//      .map { v => (v.uuid, 1) }
-//      .groupBy(0)
-//      .sum(1)
+//        .map { obj => (obj.uuid, 1)}
+//        .keyBy { _._1 }
+//        .sum(1)
 //
-//    counts.print()
+//    count.print()
   }
 }
