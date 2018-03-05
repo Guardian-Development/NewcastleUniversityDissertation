48913903139d46677dde709613a8a3b41099268e
joe <joehonour@hotmail.co.uk>
2018-03-05 16:03:29 +0000

    cleanup of adding people states

--- ActivityAnalysisService/activityanalysisservice/src/main/scala/newcastleuniversity/joehonour/Main.scala
+++ ActivityAnalysisService/activityanalysisservice/src/main/scala/newcastleuniversity/joehonour/Main.scala
@@ -1,27 +1,54 @@
 package newcastleuniversity.joehonour
 
-import newcastleuniversity.joehonour.messages.MovementObserved
+import java.util.Properties
+
+import newcastleuniversity.joehonour.messages.{DetectedObject, MovementObserved}
 import newcastleuniversity.joehonour.movement_detection.WalkingDetectors
 import newcastleuniversity.joehonour.movement_detection.detectors.{RunningDetectors, StandingDetectors}
 import newcastleuniversity.joehonour.output_streams.OutputStreams
+import org.apache.flink.streaming.api.datastream.DataStreamSink
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
 
 object Main {
 
   def main(args: Array[String]) {
 
-    //build configuration
     val properties = CommandLineParser.parseCommandLineArguments(args)
-
-    //build data source
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    //run detections
     val sourceOfDetectedObjects = env
       .addSource(InputStreams.kafkaStreamForFrameMessageTopic(properties))
       .flatMap { _.detected_objects }
 
-    //walking detector
+    //detectors
+    registerStandingPersonDetector(sourceOfDetectedObjects, properties)
+    registerWalkingPersonDetector(sourceOfDetectedObjects, properties)
+    registerRunningPersonDetector(sourceOfDetectedObjects, properties)
+
+    env.execute("object-detection-task")
+  }
+
+  def registerStandingPersonDetector(sourceOfDetectedObjects: DataStream[DetectedObject],
+                                     properties: Properties): DataStreamSink[MovementObserved] = {
+    val standingDetector = StandingDetectors
+      .standingDetectionStreamFrom(sourceOfDetectedObjects, properties)
+      .map { runningMovement => MovementObserved(
+        runningMovement.uuid,
+        runningMovement.movement_type,
+        runningMovement.fromLocationX,
+        runningMovement.fromLocationY,
+        runningMovement.toLocationX,
+        runningMovement.toLocationY,
+        runningMovement.averageDisplacement)}
+
+    standingDetector.print()
+
+    standingDetector
+      .addSink(OutputStreams.kafkaStreamForMovementObservedMessageTopic(properties))
+  }
+
+  def registerWalkingPersonDetector(sourceOfDetectedObjects: DataStream[DetectedObject],
+                                    properties: Properties): DataStreamSink[MovementObserved] = {
     val walkingDetector = WalkingDetectors
       .walkingDetectionStreamFrom(sourceOfDetectedObjects, properties)
       .map { walkingMovement => MovementObserved(
@@ -37,41 +64,24 @@ object Main {
 
     walkingDetector
       .addSink(OutputStreams.kafkaStreamForMovementObservedMessageTopic(properties))
+  }
 
-    //running detector
+  def registerRunningPersonDetector(sourceOfDetectedObjects: DataStream[DetectedObject],
+                                    properties: Properties): DataStreamSink[MovementObserved] = {
     val runningDetector = RunningDetectors
-        .runningDetectionStreamFrom(sourceOfDetectedObjects, properties)
-        .map { runningMovement => MovementObserved(
-          runningMovement.uuid,
-          runningMovement.movement_type,
-          runningMovement.fromLocationX,
-          runningMovement.fromLocationY,
-          runningMovement.toLocationX,
-          runningMovement.toLocationY,
-          runningMovement.averageDisplacement) }
+      .runningDetectionStreamFrom(sourceOfDetectedObjects, properties)
+      .map { runningMovement => MovementObserved(
+        runningMovement.uuid,
+        runningMovement.movement_type,
+        runningMovement.fromLocationX,
+        runningMovement.fromLocationY,
+        runningMovement.toLocationX,
+        runningMovement.toLocationY,
+        runningMovement.averageDisplacement) }
 
     runningDetector.print()
 
     runningDetector
       .addSink(OutputStreams.kafkaStreamForMovementObservedMessageTopic(properties))
-
-    //standing detector
-    val standingDetector = StandingDetectors
-        .standingDetectionStreamFrom(sourceOfDetectedObjects, properties)
-        .map { runningMovement => MovementObserved(
-          runningMovement.uuid,
-          runningMovement.movement_type,
-          runningMovement.fromLocationX,
-          runningMovement.fromLocationY,
-          runningMovement.toLocationX,
-          runningMovement.toLocationY,
-          runningMovement.averageDisplacement)}
-
-    standingDetector.print()
-
-    standingDetector
-      .addSink(OutputStreams.kafkaStreamForMovementObservedMessageTopic(properties))
-
-    env.execute("detection-walking-task")
   }
 }
