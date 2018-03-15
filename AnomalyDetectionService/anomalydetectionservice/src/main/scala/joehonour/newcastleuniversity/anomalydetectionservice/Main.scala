e617180b4e9adf1ddd2cad4e9f0f08a595a89664
joe <joehonour@hotmail.co.uk>
2018-03-15 10:52:50 +0000

    publishing to kafka

--- AnomalyDetectionService/anomalydetectionservice/src/main/scala/joehonour/newcastleuniversity/anomalydetectionservice/Main.scala
+++ AnomalyDetectionService/anomalydetectionservice/src/main/scala/joehonour/newcastleuniversity/anomalydetectionservice/Main.scala
@@ -1,11 +1,15 @@
 package joehonour.newcastleuniversity.anomalydetectionservice
 
+import java.util.Properties
+
 import joehonour.newcastleuniversity.anomalydetectionservice.anomaly_predictors.KMeansStreamPredictorBuilder
 import joehonour.newcastleuniversity.anomalydetectionservice.anomaly_predictors.outputs.IdentifiableVector
 import joehonour.newcastleuniversity.anomalydetectionservice.input_streams.InputStreams
-import joehonour.newcastleuniversity.anomalydetectionservice.messages.MovementObserved
+import joehonour.newcastleuniversity.anomalydetectionservice.messages.{AnomalyScore, MovementObserved}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.clustering.StreamingKMeans
+import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object Main {
@@ -16,18 +20,52 @@ object Main {
     val sparkConf = new SparkConf().setMaster(config.master()).setAppName("anomaly-detection-service")
     val context = new StreamingContext(sparkConf, Seconds(config.spark_interval()))
 
-    val inputStream = InputStreams
-      .kafkaStreamForMovementObservedMessageTopic(config, context)
-
-    KMeansStreamPredictorBuilder
+    val anomalyDetectionStream = KMeansStreamPredictorBuilder
       .buildKMeansDistancePredictor[MovementObserved](
-        inputStream,
+        InputStreams.kafkaStreamForMovementObservedMessageTopic(config, context),
         () => new StreamingKMeans().setK(config.activity_anomaly_k_amount()).setRandomCenters(6, 0, 1),
         MovementObserved.toVector,
         t => IdentifiableVector(t.uuid, MovementObserved.toVector(t)))
-      .print()
+      .map(t => AnomalyScore(t.uuid, t.distance))
+
+    OutputStreams
+      .kafkaStreamForAnomalyScore(anomalyDetectionStream, config)
 
     context.start()
     context.awaitTermination()
+  }
+}
+
+object OutputStreams {
+
+  def kafkaStreamForAnomalyScore(inputStream: DStream[AnomalyScore], config: CommandLineConfiguration): Unit = {
+    val props: Properties = kafkaProperties(config)
+
+    inputStream
+      .map(AnomalyScore.toJson)
+      .map(t => new ProducerRecord[String, Array[Byte]]("anomaly-score-output", t.getBytes))
+      .foreachRDD(t => {
+        val kafkaProducer = kafkaProducerFor(props)
+        t.foreachPartition(q => {
+          val meta = q.map(kafkaProducer.send)
+          meta.foreach { _.get() }
+        })
+      })
+  }
+
+  private def kafkaProducerFor(properties: Properties): KafkaProducer[String, Array[Byte]] = {
+    val kafkaProducer = new KafkaProducer[String, Array[Byte]](properties)
+    sys.addShutdownHook {
+      kafkaProducer.close()
+    }
+    kafkaProducer
+  }
+
+  private def kafkaProperties(config: CommandLineConfiguration): Properties = {
+    val props = new Properties()
+    props.put("bootstrap.servers", config.bootstrap_servers())
+    props.put("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+    props
   }
 }
